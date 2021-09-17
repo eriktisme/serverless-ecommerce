35e72bd7e3747bc92f24fe567e7696549bb58da5
Erik van Dam <erikvandam93@gmail.com>
2021-09-17 19:48:20 +0200

    feat: Added a products table update handler for sending out Event Bridge events

---
+++ services/products/src/table-update/handler.ts
@@ -0,0 +1,45 @@
+import { EventBridge } from 'aws-sdk'
+import { ddbToEvent, sendEvents } from '@libs/event-bridge'
+
+export function instances() {
+  return {
+    eventBridge: new EventBridge({
+      apiVersion: '2015-10-07',
+    }),
+  }
+}
+
+export async function consumer(event: any) {
+  console.debug({
+    message: 'Input event',
+    event,
+  })
+
+  const records: any[] = event['Records'] ?? []
+  console.debug({
+    message: 'Records received',
+    records: records,
+  })
+
+  const events = records.map((record) =>
+    ddbToEvent(
+      record,
+      String(process.env.EVENT_BUS_NAME),
+      `${process.env.PROJECT}.products`,
+      'Product',
+      'productId',
+      process.env._X_AMZN_TRACE_ID ?? null
+    )
+  )
+
+  console.info({
+    message: `Received ${events.length} event(s)`,
+  })
+
+  console.debug({
+    message: 'Events processed from records',
+    events,
+  })
+
+  await sendEvents(instances().eventBridge, events)
+}
