fabc33cf3dc69779ade10ad5fa9fcea8eab0bec9
Erik van Dam <erikvandam93@gmail.com>
2021-10-01 18:30:31 +0200

    feat: Ensure content is scrollable on smaller devices

--- app/src/layouts/ContentWithHeader.vue
+++ app/src/layouts/ContentWithHeader.vue
@@ -11,7 +11,13 @@ import SidebarBurger from './SidebarBurger.vue'
 
     <main>
       <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <slot />
+        <div class="flex flex-col">
+          <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <slot />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   </div>
