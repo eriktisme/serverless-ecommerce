44c2f095b49f4004282883c443e99134fb3c81c2
Erik van Dam <erikvandam93@gmail.com>
2021-10-01 16:25:37 +0200

    fix: Solved issue with login page showing sidebar

--- app/src/layouts/index.ts
+++ app/src/layouts/index.ts
@@ -1,2 +1,3 @@
+export { default as Content } from './Content.vue'
 export { default as ContentWithHeader } from './ContentWithHeader.vue'
 export { default as WithLeftSidebar } from './WithLeftSidebar.vue'
